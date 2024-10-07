ca44eca620324642fef5a205b3cbc580b4d32bba
demcps <dmarin@psnar.com>
2024-10-07 09:13:20 -0500

    first commit

---
+++ .env.ex
@@ -0,0 +1,10 @@
+EMAIL_HOST=#variable
+EMAIL_PORT=#variable
+EMAIL_USER=#variable
+EMAIL_PASS=#variable
+APP_MODE=development #or production
+DATABASE_URL="postgres://"
+
+REDIS_URL="...."
+
+MYSQL_DATABASE='test'
